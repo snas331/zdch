78d44f65ba4b710b0b96f6fb2d9fed5c8326da78
yakov23 <yakovoberderfer13@gmail.com>
2024-03-11 13:50:40 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+
+    }
+}
